a58b597159523a88c5dbffcf2f8b811e8a6fcd0b
takumines <takumines1@gmail.com>
2024-10-22 23:39:35 +0900

    DB接続処理を実装

---
+++ config/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbDatabase := os.Getenv("DB_NAME")
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
