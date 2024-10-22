f35730504582f78a0d83384af4fcd3a6e98bbc47
takumines <takumines1@gmail.com>
2024-10-22 23:39:55 +0900

    Repository層を作成

---
+++ repositories/articles.go
@@ -0,0 +1,116 @@
+package repositories
+
+import (
+	"database/sql"
+	"github.com/takumines/go-intermediate-book/models"
+)
+
+const articleNumPerPage = 5
+
+// InsertArticle 新規投稿をDBに保存する
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+	insert into articles (title, contents, username, nice, created_at) values 
+	(?, ?, ?, 0, now())
+`
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	if err != nil {
+		return models.Article{}, err
+	}
+	id, _ := result.LastInsertId()
+	newArticle.ID = int(id)
+
+	return newArticle, nil
+}
+
+// SelectArticleList 投稿一覧をDBから取得する関数
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+	select article_id, title, contents, username, nice
+	from articles
+	limit ? offset ?;
+`
+	rows, err := db.Query(sqlStr, articleNumPerPage, (page-1)*articleNumPerPage)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.Nice)
+
+		articleArray = append(articleArray, article)
+	}
+
+	return articleArray, nil
+}
+
+// SelectArticleDetail 投稿IDを指定して、記事データを取得する関数
+func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+		select *
+		from articles
+		where article_id = ?;
+	`
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.Nice, &createdTime)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	return article, nil
+}
+
+// UpdateNice いいねの数をupdateする関数
+func UpdateNice(db *sql.DB, articleID int) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	const sqlGetNice = `
+		select nice
+		from articles
+		where article_id = ?;
+	`
+	row := tx.QueryRow(sqlGetNice, articleID)
+	if err := row.Err(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	var nice int
+	err = row.Scan(&nice)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
+	_, err = tx.Exec(sqlUpdateNice, nice+1, articleID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
