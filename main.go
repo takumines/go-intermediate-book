718dc3dbfbcfd8d06496793aa835619669439032
takumines <takumines1@gmail.com>
2024-10-27 00:06:54 +0900

    アプリケーション起動時にdbインスタンスを生成し、各層の依存関係を解決するよう修正

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/takumines/go-intermediate-book/handlers"
+	"github.com/takumines/go-intermediate-book/config/database"
+	"github.com/takumines/go-intermediate-book/controllers"
+	"github.com/takumines/go-intermediate-book/services"
 
 	"log"
 	"net/http"
@@ -10,14 +12,18 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+	db, err := database.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := services.NewMyAppService(db)
+	c := controllers.NewMyAppController(s)
 
-	r.Get("/hello", handlers.HelloHandler)
-
-	r.Post("/article", handlers.PostArticleHandler)
-	r.Get("/article/list", handlers.ArticleListHandler)
-	r.Get("/article/{id:[0-9]+}", handlers.ArticleDetailHandler)
-	r.Post("/article/nice", handlers.PostNiceHandler)
-	r.Post("/comment", handlers.PostCommentHandler)
+	r.Post("/article", c.PostArticleHandler)
+	r.Get("/article/list", c.ArticleListHandler)
+	r.Get("/article/{id:[0-9]+}", c.ArticleDetailHandler)
+	r.Post("/article/nice", c.PostNiceHandler)
+	r.Post("/comment", c.PostCommentHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
