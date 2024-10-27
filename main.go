3ac2a017903fabd2f5435f6d7104fd02857e9f3b
takumines <takumines1@gmail.com>
2024-10-27 10:03:19 +0900

    router層の作成

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
 	"github.com/takumines/go-intermediate-book/config/database"
 	"github.com/takumines/go-intermediate-book/controllers"
+	"github.com/takumines/go-intermediate-book/routers"
 	"github.com/takumines/go-intermediate-book/services"
 
 	"log"
@@ -11,19 +11,13 @@ import (
 )
 
 func main() {
-	r := chi.NewRouter()
 	db, err := database.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	s := services.NewMyAppService(db)
 	c := controllers.NewMyAppController(s)
-
-	r.Post("/article", c.PostArticleHandler)
-	r.Get("/article/list", c.ArticleListHandler)
-	r.Get("/article/{id:[0-9]+}", c.ArticleDetailHandler)
-	r.Post("/article/nice", c.PostNiceHandler)
-	r.Post("/comment", c.PostCommentHandler)
+	r := routers.NewRouter(c)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
