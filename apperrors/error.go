f5cb55bfbca9799fe8d047ab94a60170fd3f80e5
takumines <takumines1@gmail.com>
2024-11-17 22:09:13 +0900

    [add] 独自エラーの作成

---
+++ apperrors/error.go
@@ -0,0 +1,26 @@
+package apperrors
+
+type MyAppError struct {
+	ErrCode
+	Message string
+	Err     error
+}
+
+// Error えらーを文字列に変換する
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+// Unwrap エラーをラップしているエラーを返す
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
+}
+
+// Wrap エラーコードとメッセージを指定してエラーをラップする
+func (code ErrCode) Wrap(err error, message string) *MyAppError {
+	return &MyAppError{
+		ErrCode: code,
+		Message: message,
+		Err:     err,
+	}
+}
---
+++ apperrors/errorcode.go
@@ -0,0 +1,12 @@
+package apperrors
+
+type ErrCode string
+
+const (
+	Unknown          ErrCode = "U000"
+	InsertDataFailed ErrCode = "S001"
+	GetDataFailed    ErrCode = "S002"
+	NAData           ErrCode = "S003"
+	NoTargetData     ErrCode = "S004"
+	UpdateDataFailed ErrCode = "S005"
+)
